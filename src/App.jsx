1dc9f98683de742c196bf6da2d91464e9e922f73
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-19 21:12:18 +0600

    feat(props): Implements props to dynamically pass value from square component under Board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 
-const App = () => {
+const Square = ({value}) => {
+return <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>{value}</button>
+}
+
+const Board = () => {
   return (
    <>
    <div>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <Square value="1"></Square>
+   <Square value="2"></Square>
+   <Square value="3"></Square>
    </div>
+
    <div>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <Square value="4"></Square>
+   <Square value="5"></Square>
+   <Square value="6"></Square>
    </div>
+
    <div>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
-   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <Square value="7"></Square>
+   <Square value="18"></Square>
+   <Square value="9"></Square>
    </div>
    </>
   )
 }
 
-export default App
+export default Board
