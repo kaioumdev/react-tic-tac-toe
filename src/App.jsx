e674f8b7d1267da5e5d33fd55cfb20649aa35ab2
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-19 21:05:51 +0600

    feat(component): create 12 squares button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const App = () => {
   return (
-    <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <>
+   <div>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   </div>
+   <div>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   </div>
+   <div>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   <button className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>x</button>
+   </div>
+   </>
   )
 }
 
