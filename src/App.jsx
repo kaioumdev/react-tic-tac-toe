a83a66a3f4f050b530fb5d62bfad0253c8d407c5
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-22 15:07:58 +0600

    feat(square): using lefting to state up method to implements square click

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from 'react'
 
-const Square = ({value}) => {
+const Square = ({value, onSquareClick}) => {
 
-return <button onClick={handleClick} className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>{value}</button>
+return <button onClick={onSquareClick} className='bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg'>{value}</button>
 }
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i){
+    const newSquares = squares.slice();
+    newSquares[i] = "x"
+    setSquares(newSquares)
+  }
   return (
    <>
    <div className='flex'>
-   <Square value={squares[0]}></Square>
-   <Square value={squares[1]}></Square>
-   <Square value={squares[2]}></Square>
+   <Square value={squares[0]} onSquareClick={() =>handleClick(0)}></Square>
+   <Square value={squares[1]} onSquareClick={() =>handleClick(1)}></Square>
+   <Square value={squares[2]} onSquareClick={() =>handleClick(2)}></Square>
    </div>
 
    <div className='flex'>
-   <Square value={squares[3]}></Square>
-   <Square value={squares[4]}></Square>
-   <Square value={squares[5]}></Square>
+   <Square value={squares[3]} onSquareClick={() =>handleClick(3)}></Square>
+   <Square value={squares[4]} onSquareClick={() =>handleClick(4)}></Square>
+   <Square value={squares[5]} onSquareClick={() =>handleClick(5)}></Square>
    </div>
 
    <div className='flex'>
-   <Square value={squares[6]}></Square>
-   <Square value={squares[7]}></Square>
-   <Square value={squares[8]}></Square>
+   <Square value={squares[6]} onSquareClick={() =>handleClick(6)}></Square>
+   <Square value={squares[7]} onSquareClick={() =>handleClick(7)}></Square>
+   <Square value={squares[8]} onSquareClick={() =>handleClick(8)}></Square>
    </div>
    </>
   )
